ebbfeaf73ec75b86b6de5f0c169687ecdbccdf6d
Noah Martin <noahm444@gmail.com>
2023-08-03 06:17:57 -1000

    Add expanding view

---
+++ DemoApp/DemoApp/TestViews/ExpandingView.swift
@@ -0,0 +1,32 @@
+//
+//  ExpandingView.swift
+//  DemoApp
+//
+//  Created by Noah Martin on 8/2/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct ExpandingView: View {
+  var body: some View {
+    List {
+      Section("First") {
+        VStack {
+          Text("Subtitle").font(.title3)
+          Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum, tortor id dapibus elementum, nibh libero pretium ligula, eu bibendum nulla sapien sit amet eros. Etiam lobortis ornare nibh, ut sagittis massa egestas sed. Donec ullamcorper consequat neque, vestibulum fringilla ipsum bibendum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam ligula nibh, scelerisque sit amet laoreet at, eleifend vel elit. Suspendisse et tellus justo. Fusce lobortis semper ipsum. Etiam sit amet ultrices velit. Aliquam iaculis faucibus maximus. Proin vel magna orci. Vestibulum auctor vel velit eu iaculis. Quisque est purus, facilisis id ligula vel, semper dictum nibh. Curabitur gravida, est et placerat iaculis, sapien sapien accumsan lacus, a congue libero quam quis sem.")
+        }
+      }
+      Section("Second") {
+        Text("Subtitle").font(.title3)
+        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum, tortor id dapibus elementum, nibh libero pretium ligula, eu bibendum nulla sapien sit amet eros. Etiam lobortis ornare nibh, ut sagittis massa egestas sed. Donec ullamcorper consequat neque, vestibulum fringilla ipsum bibendum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam ligula nibh, scelerisque sit amet laoreet at, eleifend vel elit. Suspendisse et tellus justo. Fusce lobortis semper ipsum. Etiam sit amet ultrices velit. Aliquam iaculis faucibus maximus. Proin vel magna orci. Vestibulum auctor vel velit eu iaculis. Quisque est purus, facilisis id ligula vel, semper dictum nibh. Curabitur gravida, est et placerat iaculis, sapien sapien accumsan lacus, a congue libero quam quis sem.")
+      }
+    }
+  }
+}
+
+struct ExpandingView_Previews: PreviewProvider {
+  static var previews: some View {
+    ExpandingView()
+  }
+}
