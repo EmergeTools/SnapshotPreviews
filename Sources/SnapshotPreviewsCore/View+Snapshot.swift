b827a07abf30f639b80916a7512734b47398164e
Noah Martin <noahm444@gmail.com>
2023-08-03 06:19:45 -1000

    Device layout fixes

--- Sources/SnapshotPreviewsCore/View+Snapshot.swift
+++ Sources/SnapshotPreviewsCore/View+Snapshot.swift
@@ -17,34 +17,47 @@ extension View {
     view.translatesAutoresizingMaskIntoConstraints = false
     view.backgroundColor = .clear
 
-    let windowRootVC = Self.setupRootVC(subVC: controller)
+    let (windowRootVC, containerVC) = Self.setupRootVC(subVC: controller)
     window.rootViewController = windowRootVC
     controller.expansionSettled = {
       if async {
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-          completion(Self.takeSnapshot(layout: layout, rootVC: windowRootVC, controller: controller))
+          completion(Self.takeSnapshot(layout: layout, rootVC: containerVC, controller: controller))
         }
       } else {
         DispatchQueue.main.async {
-          completion(Self.takeSnapshot(layout: layout, rootVC: windowRootVC, controller: controller))
+          completion(Self.takeSnapshot(layout: layout, rootVC: containerVC, controller: controller))
         }
       }
     }
   }
 
-  private static func setupRootVC(subVC: UIViewController) -> UIViewController {
+  private static func setupRootVC(subVC: UIViewController) -> (UIViewController, UIViewController) {
     let windowRootVC = UIViewController()
     windowRootVC.view.bounds = UIScreen.main.bounds
     windowRootVC.view.backgroundColor = .clear
-    windowRootVC.view.addSubview(subVC.view)
-    windowRootVC.addChild(subVC)
-    subVC.didMove(toParent: windowRootVC)
 
-    subVC.view.centerXAnchor.constraint(equalTo: windowRootVC.view.centerXAnchor).isActive = true
-    subVC.view.centerYAnchor.constraint(equalTo: windowRootVC.view.centerYAnchor).isActive = true
+    let containerVC = UIViewController()
+    containerVC.view.backgroundColor = .clear
+    containerVC.view.translatesAutoresizingMaskIntoConstraints = false
+    windowRootVC.view.addSubview(containerVC.view)
+    windowRootVC.addChild(containerVC)
+    containerVC.didMove(toParent: windowRootVC)
+    containerVC.view.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
+    containerVC.view.heightAnchor.constraint(greaterThanOrEqualToConstant: UIScreen.main.bounds.height).isActive = true
+    containerVC.view.centerXAnchor.constraint(equalTo: windowRootVC.view.centerXAnchor).isActive = true
+    containerVC.view.centerYAnchor.constraint(equalTo: windowRootVC.view.centerYAnchor).isActive = true
+
+    containerVC.view.addSubview(subVC.view)
+    containerVC.addChild(subVC)
+    subVC.didMove(toParent: containerVC)
+
+    subVC.view.centerXAnchor.constraint(equalTo: containerVC.view.centerXAnchor).isActive = true
+    subVC.view.centerYAnchor.constraint(equalTo: containerVC.view.centerYAnchor).isActive = true
     subVC.view.widthAnchor.constraint(lessThanOrEqualToConstant: UIScreen.main.bounds.width).isActive = true
+    containerVC.view.heightAnchor.constraint(greaterThanOrEqualTo: subVC.view.heightAnchor, multiplier: 1).isActive = true
 
-    return windowRootVC
+    return (windowRootVC, containerVC)
   }
 
   private static func takeSnapshot(layout: PreviewLayout, rootVC: UIViewController, controller: UIViewController) -> UIImage {
@@ -58,12 +71,12 @@ extension View {
     case .fixed(width: let width, height: let height):
       targetSize = CGSize(width: width, height: height)
       drawCode = { ctx in
-        controller.view.layer.layerForSnapshot.render(in: ctx)
+        view.layer.layerForSnapshot.render(in: ctx)
       }
     case .sizeThatFits:
       targetSize = view.bounds.size
       drawCode = { ctx in
-        controller.view.layer.layerForSnapshot.render(in: ctx)
+        view.layer.layerForSnapshot.render(in: ctx)
       }
     case .device:
       fallthrough
@@ -72,7 +85,7 @@ extension View {
 
       targetSize = CGSize(width: UIScreen.main.bounds.size.width, height: max(viewSize.height, UIScreen.main.bounds.size.height))
       drawCode = { ctx in
-        controller.view.layer.layerForSnapshot.render(in: ctx)
+        rootVC.view.layer.layerForSnapshot.render(in: ctx)
       }
     }
     let renderer = UIGraphicsImageRenderer(size: targetSize)
