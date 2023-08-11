d9b4c0ae3f1696dec038d292cd7a846e7e6e9aca
noahsmartin <noahm444@gmail.com>
2023-08-11 12:50:38 -0700

    Increase example height (#16)

--- DemoApp/DemoApp/TestViews/ExpandingView.swift
+++ DemoApp/DemoApp/TestViews/ExpandingView.swift
@@ -11,15 +11,13 @@ import SwiftUI
 struct ExpandingView: View {
   var body: some View {
     List {
-      Section("First") {
-        VStack {
-          Text("Subtitle").font(.title3)
-          Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum, tortor id dapibus elementum, nibh libero pretium ligula, eu bibendum nulla sapien sit amet eros. Etiam lobortis ornare nibh, ut sagittis massa egestas sed. Donec ullamcorper consequat neque, vestibulum fringilla ipsum bibendum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam ligula nibh, scelerisque sit amet laoreet at, eleifend vel elit. Suspendisse et tellus justo. Fusce lobortis semper ipsum. Etiam sit amet ultrices velit. Aliquam iaculis faucibus maximus. Proin vel magna orci. Vestibulum auctor vel velit eu iaculis. Quisque est purus, facilisis id ligula vel, semper dictum nibh. Curabitur gravida, est et placerat iaculis, sapien sapien accumsan lacus, a congue libero quam quis sem.")
+      ForEach(1..<20) { i in
+        Section("Section \(i)") {
+          VStack {
+            Text("Subtitle").font(.title3)
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum, tortor id dapibus elementum, nibh libero pretium ligula, eu bibendum nulla sapien sit amet eros. Etiam lobortis ornare nibh, ut sagittis massa egestas sed. Donec ullamcorper consequat neque, vestibulum fringilla ipsum bibendum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam ligula nibh, scelerisque sit amet laoreet at, eleifend vel elit. Suspendisse et tellus justo. Fusce lobortis semper ipsum. Etiam sit amet ultrices velit. Aliquam iaculis faucibus maximus. Proin vel magna orci. Vestibulum auctor vel velit eu iaculis. Quisque est purus, facilisis id ligula vel, semper dictum nibh. Curabitur gravida, est et placerat iaculis, sapien sapien accumsan lacus, a congue libero quam quis sem.")
+          }
         }
-      }
-      Section("Second") {
-        Text("Subtitle").font(.title3)
-        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum, tortor id dapibus elementum, nibh libero pretium ligula, eu bibendum nulla sapien sit amet eros. Etiam lobortis ornare nibh, ut sagittis massa egestas sed. Donec ullamcorper consequat neque, vestibulum fringilla ipsum bibendum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam ligula nibh, scelerisque sit amet laoreet at, eleifend vel elit. Suspendisse et tellus justo. Fusce lobortis semper ipsum. Etiam sit amet ultrices velit. Aliquam iaculis faucibus maximus. Proin vel magna orci. Vestibulum auctor vel velit eu iaculis. Quisque est purus, facilisis id ligula vel, semper dictum nibh. Curabitur gravida, est et placerat iaculis, sapien sapien accumsan lacus, a congue libero quam quis sem.")
       }
     }
   }
