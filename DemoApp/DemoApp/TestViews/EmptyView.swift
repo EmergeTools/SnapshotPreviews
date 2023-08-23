852d64fac342f8489d6d3ed650a90cb1d45f5066
noahsmartin <noahm444@gmail.com>
2023-08-22 21:23:41 -0700

    Support conditional views (#24)

---
+++ DemoApp/DemoApp/TestViews/EmptyView.swift
@@ -0,0 +1,27 @@
+//
+//  EmptyView.swift
+//  DemoApp
+//
+//  Created by Noah Martin on 8/22/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct EmptyViewTest: View {
+  var body: some View {
+    let test = true
+    if test {
+      EmptyView()
+    } else {
+      Text("Hello")
+    }
+    Text("World")
+  }
+}
+
+struct EmptyViewTest_Previews: PreviewProvider {
+  static var previews: some View {
+    EmptyViewTest()
+  }
+}
