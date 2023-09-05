326c1c10df143a8466f79a25ead416909179f53e
noahsmartin <noahm444@gmail.com>
2023-09-05 13:20:57 -0700

    lower min version (#32)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SnapshotPreviews",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
