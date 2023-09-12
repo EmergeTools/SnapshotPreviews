c9b0a33f58ebcdf9582fbea1424549ba845a5a83
Trevor Elkins <trevor@emergetools.com>
2023-09-12 13:01:42 -0400

    Update checker colors

--- Sources/PreviewGallery/PreviewCell.swift
+++ Sources/PreviewGallery/PreviewCell.swift
@@ -19,16 +19,20 @@ struct PreviewCell: View {
     VStack {
       try! preview.view()
         .padding(.vertical, 8)
-        .border(Color(UIColor.separator))
+        .border(colorScheme == .light ? Color.slate100 : Color.black)
         .background {
           Checkerboard()
-            .foregroundStyle(Color(UIColor.label))
-            .opacity(0.1)
-            .background(Color(UIColor.systemBackground))
+            .foregroundStyle(Color.lightChecker)
+            .background(colorScheme == .light ? Color.slate100 : Color.black)
         }
         .preferredColorScheme(nil)
         .colorScheme(try! preview.colorScheme() ?? colorScheme)
     }
   }
 
+}
+
+private extension Color {
+  static let lightChecker = Color(uiColor: UIColor(red: 187 / 255.0, green: 187 / 255.0, blue: 187 / 255.0, alpha: 48 / 255.0))
+  static let slate100 = Color(uiColor: UIColor(red: 241 / 255.0, green: 245 / 255.0, blue: 249 / 255.0, alpha: 1))
 }
