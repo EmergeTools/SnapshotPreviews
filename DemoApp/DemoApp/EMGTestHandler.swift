caeedf61cf8f479885d3da54f5590c62ef25079e
Noah Martin <noahm444@gmail.com>
2023-09-26 18:25:50 -0400

    Add setup

---
+++ DemoApp/DemoApp/EMGTestHandler.swift
@@ -0,0 +1,16 @@
+//
+//  EMGTestHandler.swift
+//  DemoApp
+//
+//  Created by Noah Martin on 9/26/23.
+//
+
+import Foundation
+
+@objc(EMGTestHandler)
+class EMGTestHandler: NSObject {
+  @objc
+  static func setup() {
+    print("Setup called.")
+  }
+}
