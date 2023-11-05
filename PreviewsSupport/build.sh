6ccf74cec5af2b20252fcfc2175e91c9f36c4ac7
Noah Martin <noahm444@gmail.com>
2023-11-05 09:58:07 -0500

    Add mac to previews support

--- PreviewsSupport/build.sh
+++ PreviewsSupport/build.sh
@@ -18,7 +18,17 @@ xcodebuild archive \
  SKIP_INSTALL=NO \
  CLANG_CXX_LANGUAGE_STANDARD=c++17
 
+ xcodebuild archive \
+ -scheme PreviewsSupport \
+ -archivePath ./PreviewsSupport-macosx.xcarchive \
+ -sdk macosx \
+ BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+ INSTALL_PATH='Library/Frameworks' \
+ SKIP_INSTALL=NO \
+ CLANG_CXX_LANGUAGE_STANDARD=c++17
+
 xcodebuild -create-xcframework \
  -framework ./PreviewsSupport-iphonesimulator.xcarchive/Products/Library/Frameworks/PreviewsSupport.framework \
  -framework ./PreviewsSupport-iphoneos.xcarchive/Products/Library/Frameworks/PreviewsSupport.framework \
+ -framework ./PreviewsSupport-macosx.xcarchive/Products/Library/Frameworks/PreviewsSupport.framework \
  -output ./PreviewsSupport.xcframework
