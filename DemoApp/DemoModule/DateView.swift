630cf952650c6e7957cf4d89a4ac7461cd1f96bd
Noah Martin <noahm444@gmail.com>
2023-12-28 21:30:56 -0800

    Add date view demo

---
+++ DemoApp/DemoModule/DateView.swift
@@ -0,0 +1,24 @@
+//
+//  DateView.swift
+//  DemoApp
+//
+//  Created by Noah Martin on 12/28/23.
+//
+
+import SwiftUI
+
+struct DateView: View {
+  let currentDate = Date.current
+
+    var body: some View {
+        Text(currentDate, style: .date)
+            .font(.title)
+            .padding()
+    }
+}
+
+struct DateView_Previews: PreviewProvider {
+    static var previews: some View {
+        DateView()
+    }
+}
