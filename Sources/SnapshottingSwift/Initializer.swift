49441951ac629430105b9f442e07538bcba31b0b
Noah Martin <noahm444@gmail.com>
2024-07-05 18:18:52 -0700

    delete class (#126)

--- Sources/SnapshottingSwift/Initializer.swift
+++ Sources/SnapshottingSwift/Initializer.swift
@@ -17,27 +17,3 @@ public class Initializer: NSObject {
   let snapshots = Snapshots()
 
 }
-
-private class CompletedViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        label.accessibilityIdentifier = "emg_finished_label"
-
-        label.text = "\(Snapshots.resultsDir.path)"
-
-        view.addSubview(label)
-
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            label.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
-            label.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
-        ])
-    }
-}
