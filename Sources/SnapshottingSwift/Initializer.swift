ec73bc6e6f3f8104f40559860374cc73a2f124d5
Noah Martin <noahm444@gmail.com>
2024-07-05 18:12:59 -0700

    Use more swift

--- Sources/SnapshottingSwift/Initializer.swift
+++ Sources/SnapshottingSwift/Initializer.swift
@@ -14,7 +14,14 @@ public class Initializer: NSObject {
   @objc
   static public let shared = Initializer()
 
-  let snapshots = Snapshots()
+  override init() {
+    super.init()
+
+    NotificationCenter.default.addObserver(forName: UIApplication.didBecomeActiveNotification, object: nil, queue: .main) { [weak self] notification in
+      self?.snapshots = Snapshots()
+    }
+  }
+  var snapshots: Snapshots?
 
 }
 
