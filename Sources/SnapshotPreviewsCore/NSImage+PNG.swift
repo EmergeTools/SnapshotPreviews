cfa0459718358253ba6e89606e1624322d8b4b45
Noah Martin <noahm444@gmail.com>
2024-08-02 23:44:41 -0400

    Move png data util to core

---
+++ Sources/SnapshotPreviewsCore/NSImage+PNG.swift
@@ -0,0 +1,22 @@
+//
+//  NSImage+PNG.swift
+//
+//
+//  Created by Noah Martin on 8/2/24.
+//
+
+#if canImport(AppKit)
+import AppKit
+
+extension NSImage {
+    public func pngData() -> NSData? {
+        guard let tiffData = self.tiffRepresentation,
+              let bitmapImageRep = NSBitmapImageRep(data: tiffData) else {
+            return nil
+        }
+
+        let pngData = bitmapImageRep.representation(using: .png, properties: [:])
+        return pngData as NSData?
+    }
+}
+#endif
