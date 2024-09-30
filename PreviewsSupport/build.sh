21b995116bcbdd8ba97b93b07382d35447559cef
Itay <itay@itaysoft.com>
2024-09-30 18:48:54 -0300

    Only use macos 15 and xcode 16 for testing visionOS

--- PreviewsSupport/build.sh
+++ PreviewsSupport/build.sh
@@ -112,5 +112,3 @@ xcodebuild -create-xcframework \
  -framework ./PreviewsSupport-macosx.xcarchive/Products/Library/Frameworks/PreviewsSupport.framework \
  -framework ./PreviewsSupport-catalyst.xcarchive/Products/Library/Frameworks/PreviewsSupport.framework \
  -output ./PreviewsSupport.xcframework
-
-find . -type f -name '*.swiftinterface' -exec sed -i '' 's/SwiftUICore.View/SwiftUI.View/g' {} +
