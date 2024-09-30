c8c500378700c05354816172209bf6c25e91d289
Trevor Elkins <trevor@emergetools.com>
2024-09-30 14:43:01 -0400

    fix

--- Sources/SnapshotPreferences/AppStoreScreenshotPreference.swift
+++ Sources/SnapshotPreferences/AppStoreScreenshotPreference.swift
@@ -32,8 +32,8 @@ extension View {
     /// ```swift
     /// struct ContentView: View {
     ///     var body: some View {
-    ///         Text("Accessible Content")
-    ///             .emergeAccessibility(true)
+    ///         Text("A beautiful screenshot")
+    ///             .emergeAppStoreScreenshot(true)
     ///     }
     /// }
     /// ```
