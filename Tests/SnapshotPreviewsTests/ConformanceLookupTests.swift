5687cc5410b1e44e4346166a4c2e655209e4c98d
Trevor Elkins <trevor@emergetools.com>
2025-04-30 15:47:02 -0400

    fix

--- Tests/SnapshotPreviewsTests/ConformanceLookupTests.swift
+++ Tests/SnapshotPreviewsTests/ConformanceLookupTests.swift
@@ -1,26 +1,15 @@
-import SwiftUI
 import XCTest
 
 @testable import SnapshotPreviewsCore
 
-struct TestView: View {
-  var body: some View {
-    Text("Hello world!")
-  }
-}
-
-#Preview {
-  TestView()
-}
-
 final class ConformanceLookupTests: XCTestCase {
-  func testExample() throws {
+  func testGetPreviewTypes() throws {
     let types = ConformanceLookup.getPreviewTypes()
     XCTAssertEqual(types.count, 1)
 
     let firstType = types.first!
     XCTAssertEqual(
-      "SnapshotPreviewsTests.$s21SnapshotPreviewsTests0033ConformanceLookupTestsswift_DbGHjfMX11_0_33_AB2146FE95919420F4A1C0A89BE8EA36Ll7PreviewfMf_15PreviewRegistryfMu_",
+      "SnapshotPreviewsTests.$s21SnapshotPreviewsTests0019TestViewswift_DJEEdfMX15_0_33_B5E96601318DE1EC85533DD88EB53190Ll7PreviewfMf_15PreviewRegistryfMu_",
       firstType.name
     )
     XCTAssertEqual("PreviewRegistry", firstType.proto)
